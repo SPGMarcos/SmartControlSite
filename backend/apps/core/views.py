b25a6c820f3ec3ba6b0248acebe809c56f325325
Marcos Gabriel <144981603+SPGMarcos@users.noreply.github.com>
2026-08-11 17:23:41 -0300

    Remover throttle do health check

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,7 +1,7 @@
 from django.middleware.csrf import get_token
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, throttle_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
@@ -9,11 +9,13 @@ from rest_framework.response import Response
 @ensure_csrf_cookie
 @api_view(["GET"])
 @permission_classes([AllowAny])
+@throttle_classes([])
 def csrf(request):
     return JsonResponse({"csrfToken": get_token(request)})
 
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
+@throttle_classes([])
 def health(request):
     return Response({"status": "ok"})
